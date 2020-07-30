13a970d25cbf50f5c836e55d85cb6e41c7693998
CasperCarver <hleb.piguleuski@gmail.com>
2020-07-30 13:57:18 +0300

    feat: add modal img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './comps/Title';
 import UploadForms from './comps/UploadForms';
 import ImageGrid from './comps/ImageGrid';
+import Modal from './comps/Modal';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className='App'>
       <Title />
       <UploadForms />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
